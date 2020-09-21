13962bf3fd3ddac5a2a4edcdc391d7c7d8be2470
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-21 21:52:15 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 # describe '#reverse_each_word' do
 #   let(:sentence1) { "Hello there, and how are you?" }
-#   it 'reverses all the words in a string without reversing the order of the words' do
-#     expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna woh era ?uoy")
+#   it 'reverses all the words in a string without reversing the #order of the words' do
+#     expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna # era ?uoy")
 # end 
 # end 
 
